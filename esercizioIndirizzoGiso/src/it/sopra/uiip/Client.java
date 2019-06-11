ef9cc938b67039d1d4313dc7180e4e3a690f0618
TommasoMinichiello <t.minichiello05@gmail.com>
2019-06-11 12:52:18 +0200

    Aggiunta istanza di persona chiamata tommaso

--- esercizioIndirizzoGiso/src/it/sopra/uiip/Client.java
+++ esercizioIndirizzoGiso/src/it/sopra/uiip/Client.java
@@ -1,9 +1,13 @@
 package it.sopra.uiip;
 
+import java.util.Date;
+
 public class Client {
 
 	public static void main(String[] args) {
-		
+		Indirizzo indi=new Indirizzo("c.da sant'andrea", 21, "Grottaminarda", 83035, "Avellino", "Campania");
+		Persona tommaso=new Persona("tommaso", "minichiello", new Date(05, 01, 1994), indi);
+		System.out.println(tommaso.toString());
 	}
 
 }
