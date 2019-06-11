ab0e43c83e2e636bd37132ff44a56ee7d5f7c813
DaniloFerraragit config --global user.name DaniloFerraragit config --global user.name DaniloFerrara <ferrara.danilo87@gmail.com>
2019-06-11 12:55:08 +0200

    istanza Persona

--- Indirizzo/src/it/sopra/uiip/Client.java
+++ Indirizzo/src/it/sopra/uiip/Client.java
@@ -4,6 +4,11 @@ public class Client {
 
 	public static void main(String[] args) {
 		
+		Indirizzo i1 = new Indirizzo("Via dei Santini",15,"Cesinali",83020,"Avellino","Campania");
+		
+		Persona danilo = new Persona("Danilo","Ferrara",i1);
+		
+		System.out.println(danilo.toString());
 
 	}
 
