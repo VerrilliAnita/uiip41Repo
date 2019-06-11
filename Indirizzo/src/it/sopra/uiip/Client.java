4ee8eaf663329873b143d39f6d04f1da8c78599b
EmanueleMorante <emanuele.morante@live.it>
2019-06-11 12:50:53 +0200

    aggiunto in Client.java l'oggetto morich

--- Indirizzo/src/it/sopra/uiip/Client.java
+++ Indirizzo/src/it/sopra/uiip/Client.java
@@ -4,7 +4,10 @@ public class Client {
 
 	public static void main(String[] args) {
 		
-
+		Persona morich = new Persona("Emanuele", "Morante", new Indirizzo("Atlantici", 16, "Benevento", 82100, "BN", "Campania"));
+		
+		System.out.println(morich.toString());
+		
 	}
 
 }
