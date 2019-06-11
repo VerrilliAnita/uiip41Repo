26d4a062ae0775eaba09182e9bf044c0ac62dbc5
GianlucaGiso <gianlucagiso@gmail.com>
2019-06-11 12:45:00 +0200

    creata la classe Persona

---
+++ esercizioIndirizzoGiso/src/it/sopra/uiip/Persona.java
@@ -0,0 +1,65 @@
+package it.sopra.uiip;
+
+import java.util.Date;
+
+public class Persona {
+	
+	private String nome;
+	private String cognome;
+	private Date dataDiNascita;
+	private Indirizzo indirizzo;
+	
+	public Persona(String nome, String cognome, Date dataDiNascita, Indirizzo indirizzo) {
+		
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataDiNascita = dataDiNascita;
+		this.indirizzo = indirizzo;
+	}
+	
+	public Persona() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDataDiNascita() {
+		return dataDiNascita;
+	}
+
+	public void setDataDiNascita(Date dataDiNascita) {
+		this.dataDiNascita = dataDiNascita;
+	}
+
+	public Indirizzo getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(Indirizzo indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nome=" + nome + ", cognome=" + cognome + ", dataDiNascita=" + dataDiNascita + ", indirizzo="
+				+ indirizzo.toString() + "]";
+	}
+	
+	
+	
+
+}
