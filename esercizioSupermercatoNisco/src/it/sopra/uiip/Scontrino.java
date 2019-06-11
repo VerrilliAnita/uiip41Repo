8d7512faca063e6d0cbc1d81ac713889b9bac5ca
GiuseppeRistaino <giuseppe.ristaino@gmail.com>
2019-06-11 16:01:52 +0200

    Classe Scontrino

---
+++ esercizioSupermercatoNisco/src/it/sopra/uiip/Scontrino.java
@@ -0,0 +1,59 @@
+package it.sopra.uiip;
+
+import java.util.ArrayList;
+
+public class Scontrino {
+	
+	private String codice;
+	private Cliente cliente;
+	private ArrayList<Prodotto> listaProdotti;
+	private double importo;
+	
+	public Scontrino(String codice, Cliente cliente, ArrayList<Prodotto> listaProdotti, double importo) {
+		super();
+		this.codice = codice;
+		this.cliente = cliente;
+		this.listaProdotti = listaProdotti;
+		this.importo = importo;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public ArrayList<Prodotto> getListaProdotti() {
+		return listaProdotti;
+	}
+
+	public void setListaProdotti(ArrayList<Prodotto> listaProdotti) {
+		this.listaProdotti = listaProdotti;
+	}
+	
+	public double getImporto() {
+		return importo;
+	}
+
+	public void setImporto(double importo) {
+		this.importo = importo;
+	}
+
+	@Override
+	public String toString() {
+		return "Scontrino [codice=" + codice + ", cliente=" + cliente + "]";
+	}
+	
+	
+
+}
