50f3b12eb8d4cd97b403af9d14ac604740a5e630
TommasoMinichiello <t.minichiello05@gmail.com>
2019-06-11 16:13:49 +0200

    Aggiunto un cliente e quattro istanze di prodotto

--- EsercizioSupermercatoGiso/src/it/sopra/uiip/Principale.java
+++ EsercizioSupermercatoGiso/src/it/sopra/uiip/Principale.java
@@ -3,6 +3,11 @@ package it.sopra.uiip;
 public class Principale {
 
 	public static void main(String[] args) {
+		Cliente c1=new Cliente("c01", "tommaso", "minichiello", 0);
+		Prodotto p1=new Prodotto("01", "pane", 2.0, 10);
+		Prodotto p2=new Prodotto("02", "farina", 1.0, 10);
+		Prodotto p3=new Prodotto("03", "tonno", 3.0, 15);
+		Prodotto p4=new Prodotto("04", "latte", 1.0, 30);
 		
 	}
 
