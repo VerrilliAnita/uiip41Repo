2211e544ea766a2e2b9b35c0e8d8f21750c88ac7
GoncharOksana <kseniagonchar080486@gmail.com>
2019-06-11 12:57:44 +0200

    istanza Gonchar

--- esercizioIndirizzoNisco/src/it/sopra/uiip/Client.java
+++ esercizioIndirizzoNisco/src/it/sopra/uiip/Client.java
@@ -1,8 +1,13 @@
 package it.sopra.uiip;
 
+import java.util.Date;
+
 public class Client {
 
 	public static void main(String[] args) {
+		Persona oksana =new Persona ("Oksana", "Gonchar", new Date(1986, 8, 4), new Indirizzo ("via Torreamando", 19,  "Ariano", 83010, "AV","Campania"));
+		
+		System.out.println(oksana.toString());
 
 	}
 
