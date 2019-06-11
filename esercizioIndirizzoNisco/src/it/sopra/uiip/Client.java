990273a1d5d80075c69f224bfa584b11dc8062b2
NiscoAntonio <soprasteria@ITEM-S81990.emea.msad.sopra>
2019-06-11 13:01:31 +0200

    istanza Nisco

--- esercizioIndirizzoNisco/src/it/sopra/uiip/Client.java
+++ esercizioIndirizzoNisco/src/it/sopra/uiip/Client.java
@@ -1,9 +1,12 @@
 package it.sopra.uiip;
 
+import java.util.Date;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Persona antonio= new Persona("Antonio","Nisco",new Date(1996,10,9),new Indirizzo("Tufi",47,"Zungoli",83030,"AV","Campania"));
+		System.out.println(antonio.toString());
 
 	}
 
