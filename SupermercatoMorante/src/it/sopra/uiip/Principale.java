86a978217baf5677f3780f2ae4e84ca29da318ca
DaniloFerraragit config --global user.name DaniloFerraragit config --global user.name DaniloFerrara <ferrara.danilo87@gmail.com>
2019-06-11 17:15:18 +0200

    metodi principale

--- SupermercatoMorante/src/it/sopra/uiip/Principale.java
+++ SupermercatoMorante/src/it/sopra/uiip/Principale.java
@@ -1,8 +1,12 @@
 package it.sopra.uiip;
 
+import java.util.ArrayList;
+
 public class Principale {
 
 	public static void main(String[] args) {
+		
+		ArrayList<Prodotto> listaProdottiFerrara = new ArrayList<Prodotto>();
 		
 		Cliente Suglia = new Cliente("a01", "Pompilio", "Suglia", 122);		
 		Cliente morich = new Cliente("a03", "Morante", "Emanuele", 134);
@@ -17,6 +21,27 @@ public class Principale {
 		Prodotto p7 = new Prodotto("MMM", "tonno", 2.6, 34);
 		Prodotto p8 = new Prodotto("OOO", "senape", 2.4, 18);
 		Prodotto p9 = new Prodotto("RRR", "cereali", 4.5, 40);
+		
+		
+		listaProdottiFerrara.add(p4);
+		listaProdottiFerrara.add(p5);
+		listaProdottiFerrara.add(p6);
+		
+		double contoFerrara = Supermercato.calcolaImporto(listaProdottiFerrara);
+		
+		Scontrino s1 = new Scontrino("S01",Ferrara,contoFerrara);
+		
+		s1.addProdotto(p4);
+		s1.addProdotto(p5);
+		s1.addProdotto(p6);
+		
+		//Calcolo punti scontrino Ferrara
+		System.out.println("I punti per lo scontrino s1 sono: "+ Supermercato.calcolaPunti(s1));
+		//Calcolo importo prodotti Ferrara
+		System.out.println("L'importo totale per i prodotti Ferrara è: "+ Supermercato.calcolaImporto(listaProdottiFerrara));
+		
+		
+		
 		
 
 	}
--- SupermercatoMorante/src/it/sopra/uiip/Supermercato.java
+++ SupermercatoMorante/src/it/sopra/uiip/Supermercato.java
@@ -12,13 +12,11 @@ public class Supermercato {
 
 	}
 
-
 	public static double calcolaImporto(ArrayList<Prodotto> prodotti) {
 		double conto = 0;
 		for(Prodotto p : prodotti)
 			conto = conto+p.getCosto();
 		return conto;
 	}
-
 
 }
