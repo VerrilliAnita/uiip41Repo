54ac0b2f776c9034edeaa188e98d915dc3b7e67b
GianlucaGiso <gianlucagiso@gmail.com>
2019-06-11 13:30:07 +0200

    Istanziata nella classe Client.java la classe Studente.java

--- esercizioIndirizzoGiso/src/it/sopra/uiip/Client.java
+++ esercizioIndirizzoGiso/src/it/sopra/uiip/Client.java
@@ -10,12 +10,19 @@ public class Client {
 		ArrayList<Persona> listaPersone = new ArrayList<Persona>();
 		Persona p1 = new Persona("Gianluca","Giso",new Date(11, 06, 2019),
 				new Indirizzo("Via Carlo Alberto Dalla Chiesa",36,"Vallata",83059,"Avellino","Campania"));
+		
+		
+		ArrayList<String> corsiS1 = new ArrayList<String>();
+		corsiS1.add("Ingegneria del Software");
+		corsiS1.add("Tecniche di Programmazione");
+		corsiS1.add("ASSD");
+		Studente s1 = new Studente(p1,2,"Ingegneria Informatica",corsiS1);
 		System.out.println(p1.toString());
+		System.out.println(s1.toString());
+		
 		Indirizzo indi=new Indirizzo("c.da sant'andrea", 21, "Grottaminarda", 83035, "Avellino", "Campania");
 		Persona tommaso=new Persona("tommaso", "minichiello", new Date(05, 01, 1994), indi);
 		System.out.println(tommaso.toString());
-		
-
 		listaPersone.add(p1);
 		listaPersone.add(tommaso);
 		
