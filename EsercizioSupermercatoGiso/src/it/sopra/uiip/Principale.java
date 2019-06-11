76f004d5fd34daa2a339a1b2a97569a6d6699484
GianlucaGiso <gianlucagiso@gmail.com>
2019-06-11 16:18:33 +0200

    aggiunti istanze di prodotto e client nella classe Principale

--- EsercizioSupermercatoGiso/src/it/sopra/uiip/Principale.java
+++ EsercizioSupermercatoGiso/src/it/sopra/uiip/Principale.java
@@ -4,6 +4,13 @@ public class Principale {
 
 	public static void main(String[] args) {
 		
+		Cliente c1 = new Cliente("cartaGiso", "Gianluca", "Giso", 150);
+		
+		Prodotto p1 = new Prodotto("id1","zucchero",0.99,1);
+		Prodotto p2 = new Prodotto("id2","pasta",1.99,2);
+		Prodotto p3 = new Prodotto("id3","tovaglioli",1.59,1);
+		
+		
 	}
 
 }
