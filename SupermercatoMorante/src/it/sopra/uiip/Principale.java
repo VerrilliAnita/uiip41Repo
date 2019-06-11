ebe041e3952bcf302282d4519eb130943f393103
EmanueleMorante <emanuele.morante@live.it>
2019-06-11 16:21:10 +0200

    aggiunto cliente e prodotti

--- SupermercatoMorante/src/it/sopra/uiip/Principale.java
+++ SupermercatoMorante/src/it/sopra/uiip/Principale.java
@@ -7,12 +7,16 @@ public class Principale {
 		Cliente Suglia = new Cliente("a01", "Pompilio", "Suglia", 122);
 		
 		
-		
+		Cliente morich = new Cliente("a03", "Morante", "Emanuele", 134);
 		
 		Prodotto p1 = new Prodotto("A01", "mele", 2.3, 20);
 		Prodotto p2 = new Prodotto("A02", "pane", 1.8, 10);
 		Prodotto p3 = new Prodotto("A02", "biscotti", 2, 15);
 
+		
+		Prodotto p7 = new Prodotto("MMM", "tonno", 2.6, 34);
+		Prodotto p8 = new Prodotto("OOO", "senape", 2.4, 18);
+		Prodotto p9 = new Prodotto("RRR", "cereali", 4.5, 40);
 
 	
 
