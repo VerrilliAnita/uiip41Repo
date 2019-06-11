6bc543d14d7d7891d1d3868dc572a55ff302ae30
SugliaPompilio <pompiliosuglia89@gmail.com>
2019-06-11 16:13:26 +0200

    cliente suglia e prodotti

---
+++ SupermercatoMorante/src/it/sopra/uiip/Principale.java
@@ -0,0 +1,21 @@
+package it.sopra.uiip;
+
+public class Principale {
+
+	public static void main(String[] args) {
+		
+		Cliente Suglia = new Cliente("a01", "Pompilio", "Suglia", 122);
+		
+		
+		
+		
+		Prodotto p1 = new Prodotto("A01", "mele", 2.3, 20);
+		Prodotto p2 = new Prodotto("A02", "pane", 1.8, 10);
+		Prodotto p3 = new Prodotto("A02", "biscotti", 2, 15);
+
+
+	
+
+	}
+
+}
