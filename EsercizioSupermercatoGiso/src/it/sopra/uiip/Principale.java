6bc5a7dfaf115f5ee6301887bf59a8104a094118
GianlucaGiso <gianlucagiso@gmail.com>
2019-06-11 16:21:48 +0200

    create istanze di prodotto e cliente in Principale

--- EsercizioSupermercatoGiso/src/it/sopra/uiip/Principale.java
+++ EsercizioSupermercatoGiso/src/it/sopra/uiip/Principale.java
@@ -9,11 +9,11 @@ public class Principale {
 		Prodotto p3=new Prodotto("03", "tonno", 3.0, 15);
 		Prodotto p4=new Prodotto("04", "latte", 1.0, 30);
 		
-		Cliente c1 = new Cliente("cartaGiso", "Gianluca", "Giso", 150);
+		Cliente c2 = new Cliente("cartaGiso", "Gianluca", "Giso", 150);
 		
-		Prodotto p1 = new Prodotto("id1","zucchero",0.99,1);
-		Prodotto p2 = new Prodotto("id2","pasta",1.99,2);
-		Prodotto p3 = new Prodotto("id3","tovaglioli",1.59,1);
+		Prodotto p5 = new Prodotto("id1","zucchero",0.99,1);
+		Prodotto p6 = new Prodotto("id2","pasta",1.99,2);
+		Prodotto p7 = new Prodotto("id3","tovaglioli",1.59,1);
 		
 		
 	}
