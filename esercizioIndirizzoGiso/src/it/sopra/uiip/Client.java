03fde6f738ef0d5558207683ada8df6a87b1e285
GianlucaGiso <gianlucagiso@gmail.com>
2019-06-11 12:53:52 +0200

    creata un istanza della classe Persona all'interno di Client.java

--- esercizioIndirizzoGiso/src/it/sopra/uiip/Client.java
+++ esercizioIndirizzoGiso/src/it/sopra/uiip/Client.java
@@ -1,8 +1,15 @@
 package it.sopra.uiip;
 
+import java.util.Date;
+
 public class Client {
 
 	public static void main(String[] args) {
+		
+		Persona p1 = new Persona("Gianluca","Giso",new Date(11, 06, 2019),
+				new Indirizzo("Via Carlo Alberto Dalla Chiesa",36,"Vallata",83059,"Avellino","Campania"));
+		
+		System.out.println(p1.toString());
 		
 	}
 
