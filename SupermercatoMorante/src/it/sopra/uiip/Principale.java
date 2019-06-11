178f71f5495461cc1faa232506c573e4e96027ee
SugliaPompilio <pompiliosuglia89@gmail.com>
2019-06-11 17:11:05 +0200

    test metodi Supoermercato

--- SupermercatoMorante/src/it/sopra/uiip/Principale.java
+++ SupermercatoMorante/src/it/sopra/uiip/Principale.java
@@ -1,8 +1,13 @@
 package it.sopra.uiip;
 
+import java.util.ArrayList;
+
 public class Principale {
 
 	public static void main(String[] args) {
+		
+		ArrayList<Prodotto>listaProdottiSuglia=new ArrayList<Prodotto>();
+		int puntiSuglia=0;
 		
 		Cliente Suglia = new Cliente("a01", "Pompilio", "Suglia", 122);		
 		Cliente morich = new Cliente("a03", "Morante", "Emanuele", 134);
@@ -17,6 +22,21 @@ public class Principale {
 		Prodotto p7 = new Prodotto("MMM", "tonno", 2.6, 34);
 		Prodotto p8 = new Prodotto("OOO", "senape", 2.4, 18);
 		Prodotto p9 = new Prodotto("RRR", "cereali", 4.5, 40);
+		
+		
+		listaProdottiSuglia.add(p5);
+		listaProdottiSuglia.add(p2);
+		listaProdottiSuglia.add(p1);
+		
+		double contoSuglia = Supermercato.calcolaImporto(listaProdottiSuglia);
+		Scontrino scontrinoSuglia = new Scontrino("s1", Suglia, contoSuglia);
+		scontrinoSuglia.setProdotti(listaProdottiSuglia);
+		puntiSuglia = Supermercato.calcolaPunti(scontrinoSuglia);
+		System.out.println("\n------------Scontrino Suglia-----------");
+		System.out.println("conto scontrino Suglia = " + contoSuglia + "Euro");
+		System.out.println("Punti scontrino Suglia = " + puntiSuglia+"\n");
+		
+		
 		
 
 	}
