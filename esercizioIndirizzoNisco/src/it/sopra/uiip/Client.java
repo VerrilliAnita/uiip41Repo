01400758ba8b16f4ad71b85a692aeb3b255e14fc
GiuseppeRistaino <giuseppe.ristaino@gmail.com>
2019-06-11 12:56:01 +0200

    Aggiunta istanza persona al main

--- esercizioIndirizzoNisco/src/it/sopra/uiip/Client.java
+++ esercizioIndirizzoNisco/src/it/sopra/uiip/Client.java
@@ -1,9 +1,16 @@
 package it.sopra.uiip;
 
+import java.util.Date;
+
 public class Client {
 
 	public static void main(String[] args) {
 
+		Persona giuseppe = new Persona("Giuseppe", "Ristaino", new Date(1989, 4, 25), 
+				new Indirizzo("Via prov. Cerzete", 22, "Atripalda", 83042, "AV", 
+						"Campania"));
+		
+		System.out.println(giuseppe.toString());
 	}
 
 }
