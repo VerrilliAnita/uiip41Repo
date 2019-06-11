5f7bd4a2d0c8e4e6cc77a948bbdae281567186e9
SugliaPompilio <pompiliosuglia89@gmail.com>
2019-06-11 12:49:29 +0200

    istanza di Suglia

--- Indirizzo/src/it/sopra/uiip/Client.java
+++ Indirizzo/src/it/sopra/uiip/Client.java
@@ -4,6 +4,10 @@ public class Client {
 
 	public static void main(String[] args) {
 		
+		Indirizzo i1 = new Indirizzo("Via Roma", 23, "Roma", 80045, "Roma", "Lazio");
+		Persona suglia = new Persona("Pompilio", "Suglia", i1);
+		
+		System.out.println(suglia.toString());
 
 	}
 
