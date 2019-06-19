be0388624b7c3e6c181359a996b4d0807f2c6996
SugliaPompilio <pompiliosuglia89@gmail.com>
2019-06-19 02:00:40 +0200

    Homework2 Suglia

---
+++ hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/handler/AgePassengerHandler.java
@@ -0,0 +1,23 @@
+package it.uiip.airport.core.handler;
+
+import java.util.Date;
+
+import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
+
+import it.uiip.airport.core.model.PasseggeroModel;
+
+public class AgePassengerHandler implements DynamicAttributeHandler<String, PasseggeroModel>{
+
+	@Override
+	public String get(PasseggeroModel passeggero) {
+		return Integer.toString(passeggero.getCurrentDate().getYear()-new Date(passeggero.getDataNascita()).getYear());
+	}
+
+	@Override
+	public void set(PasseggeroModel passeggero, String eta) {
+		passeggero.setEta(Integer.parseInt(eta));
+	}
+
+	
+
+}
