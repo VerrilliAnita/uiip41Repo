e217422c4fcb29747c6c6cdc909c9935524f7d73
TommasoMinichiello <t.minichiello05@gmail.com>
2019-06-20 10:44:41 +0200

    modify DefaultPassengerDao

--- hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/dao/impl/DefaultPassengerDao.java
+++ hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/dao/impl/DefaultPassengerDao.java
@@ -4,6 +4,8 @@
 package it.uiip.airport.core.dao.impl;
 
 import de.hybris.platform.servicelayer.internal.dao.DefaultGenericDao;
+import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
+import de.hybris.platform.servicelayer.search.SearchResult;
 
 import java.util.List;
 
@@ -35,7 +37,17 @@ public class DefaultPassengerDao extends DefaultGenericDao<PassengerModel> imple
 	public List<PassengerModel> findPassengersByFlightDay(final String day)
 	{
 
-		return null;
+		final StringBuilder queryString = new StringBuilder();
+		queryString.append("SELECT {P.pk}");
+		queryString.append("FROM {Passenger AS P");
+		queryString.append("JOIN Pass2Rout AS PR ON {PR.passenger} = {P.pk}");
+		queryString.append("JOIN Rout AS R ON {PR.route} = {R.pk}");
+		queryString.append("JOIN Flight F ON {R.flight} = {F.pk}}");
+		queryString.append("WHERE {F.weekday} = ?day");
+		final FlexibleSearchQuery fsq = new FlexibleSearchQuery(queryString);
+		fsq.addQueryParameter("day", day);
+		final SearchResult<PassengerModel> result = getFlexibleSearchService().search(fsq);
+		return result.getResult();
 	}
 
 
