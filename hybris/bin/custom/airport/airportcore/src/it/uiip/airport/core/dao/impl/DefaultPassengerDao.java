7a05e7bd357a9bdd0605cc70d753bcd4369682a1
TommasoMinichiello <t.minichiello05@gmail.com>
2019-06-20 10:24:07 +0200

    modify bean and relation

--- hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/dao/impl/DefaultPassengerDao.java
+++ hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/dao/impl/DefaultPassengerDao.java
@@ -28,13 +28,13 @@ public class DefaultPassengerDao extends DefaultGenericDao<PassengerModel> imple
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see it.uiip.airport.core.dao.PassengerDao#findPassengerByFlightDay(java.lang.String)
 	 */
 	@Override
-	public List<PassengerModel> findPassengerByFlightDay(final String day)
+	public List<PassengerModel> findPassengersByFlightDay(final String day)
 	{
-		// XXX Auto-generated method stub
+
 		return null;
 	}
 
