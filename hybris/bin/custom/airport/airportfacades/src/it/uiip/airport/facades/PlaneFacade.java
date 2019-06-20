fd4df108006dfe8cea381ca2e2c8a7789fb8889c
GiuseppeRistaino <giuseppe.ristaino@gmail.com>
2019-06-20 23:33:25 +0200

    Update airport facade

---
+++ hybris/bin/custom/airport/airportfacades/src/it/uiip/airport/facades/PlaneFacade.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package it.uiip.airport.facades;
+
+import java.util.List;
+
+import it.uiip.airport.facades.data.PlaneData;
+
+
+/**
+ * @author soprasteria
+ *
+ */
+public interface PlaneFacade
+{
+	public List<PlaneData> getAllPlane();
+}
