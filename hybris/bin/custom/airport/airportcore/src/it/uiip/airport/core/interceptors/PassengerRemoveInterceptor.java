2a87394d170f8632bea0e1ce4ec18cfde88f3f80
GiuseppeRistaino <giuseppe.ristaino@gmail.com>
2019-06-24 18:17:11 +0200

    Added PassengerRemoveInterceptor

---
+++ hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/interceptors/PassengerRemoveInterceptor.java
@@ -0,0 +1,45 @@
+package it.uiip.airport.core.interceptors;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Required;
+
+import de.hybris.platform.servicelayer.interceptor.InterceptorContext;
+import de.hybris.platform.servicelayer.interceptor.InterceptorException;
+import de.hybris.platform.servicelayer.interceptor.RemoveInterceptor;
+import it.uiip.airport.core.model.PassengerModel;
+import it.uiip.airport.core.model.TicketModel;
+import it.uiip.airport.core.service.TicketPlaneService;
+
+public class PassengerRemoveInterceptor implements RemoveInterceptor {
+
+	@Resource
+	private TicketPlaneService ticketPlaneService; 
+	
+	@Override
+	public void onRemove(Object model, InterceptorContext ctx) throws InterceptorException {
+		if(model instanceof PassengerModel && model != null) {
+			PassengerModel passenger = (PassengerModel) model;
+			List<TicketModel> tickets = ticketPlaneService.getTicketsForPassenger(passenger.getUid());
+			for(TicketModel ticket : tickets) {
+				if(ticket.getRoute().getDateRouteDep().after(new Date()))
+					ctx.getModelService().remove(ticket);
+			}
+		}
+	}
+
+	public TicketPlaneService getTicketPlaneService() {
+		return ticketPlaneService;
+	}
+
+	@Required
+	public void setTicketPlaneService(TicketPlaneService ticketPlaneService) {
+		this.ticketPlaneService = ticketPlaneService;
+	}
+
+	
+	
+}
