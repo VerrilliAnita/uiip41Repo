be75e3a446270997c34d43aacd9764a7b354dc43
DaniloFerraragit config --global user.name DaniloFerraragit config --global user.name DaniloFerrara <ferrara.danilo87@gmail.com>
2019-06-26 23:42:44 +0200

    BeanShell like BeanShell_Minichiello

--- hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/dao/AirportRouteDao.java
+++ hybris/bin/custom/airport/airportcore/src/it/uiip/airport/core/dao/AirportRouteDao.java
@@ -3,6 +3,7 @@
  */
 package it.uiip.airport.core.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import it.uiip.airport.core.model.RouteModel;
@@ -19,4 +20,6 @@ public interface AirportRouteDao
 	public List<RouteModel> findAirportRouteByCode(String codeRoute);
 
 	public List<RouteModel> findAirportRouteDeleted(String status);
+	
+	public List<RouteModel> findFlightRouteByCityAndDay(String city, Date date);
 }
